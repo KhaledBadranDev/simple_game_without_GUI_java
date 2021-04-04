11773e00022a0adabaa8b5e0c6d143980e67977d
Khaled Badran <gym4programming@gmail.com>
2021-04-04 14:45:27 +0200

    create the game main folder

---
+++ game/Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author Khaled Badran
+ */
+
+package game;
+
+import game.simulation.Simulation;
+
+class Main {
+    
+    public static void main(String[] args) {
+        Simulation game = new Simulation();                     
+        game.run();        
+    }
+}
